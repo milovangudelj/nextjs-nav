fb3a560cab728c219cf5bdd4a936fc19d47f8bb7
romiem <rohit.mistry@gmail.com>
2024-02-29 11:40:19 +0000

    feat: test

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import styles from "./page.module.css";
 import Link from "next/link";
 
 export default function Home() {
+
   return (
-    <main className={styles.main}>
+    <main>
       <div>Home page</div>
       <Link href='/'>Home</Link>
       <Link href='/about'>About</Link>
